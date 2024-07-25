76ab7946040da24cc1ba346583af6c06ca5b2884
k35havporwal <145449003+k35havporwal@users.noreply.github.com>
2024-07-25 15:08:21 +0530

    Create 4. Pattern-4.cpp

---
+++ Step-2 : Build-up Logical Thinking/4. Pattern-4.cpp
@@ -0,0 +1,27 @@
+/*
+
+1
+2 2 
+3 3 3
+
+*/
+
+#include <iostream>
+using namespace std;
+
+void pattern4(int n){
+  for (int i=1 ; i<=n ; i++){
+    for (int j=1 ; j<=i ; j++){
+      cout << i << " ";
+    }
+    cout << endl;
+  }
+}
+
+int main (){
+  int n;
+  cout << "Enter value of N : ";
+  cin >> n;
+  pattern4(n);
+  return 0;
+}
