d0a0669156a3bf6604ce8520c031fab54e620721
k35havporwal <145449003+k35havporwal@users.noreply.github.com>
2024-07-25 12:24:00 +0530

    Update 3. Switch Statement.cpp

--- Step-1 : Learn Basics/3. Switch Statement.cpp
+++ Step-1 : Learn Basics/3. Switch Statement.cpp
@@ -1,4 +1,12 @@
+/*
+Given an integer choice denoting the choice of the user and a list containing the single value R or two values L and B depending on the choice.
+If the user's choice is 1, calculate the area of the circle having the given radius(R).  
+Else if the choice is 2, calculate the area of the rectangle with the given length(L) and breadth(B).
+*/
 
+#include <iostream>
+#include <cmath>
+using namespace std;
 class Solution {
   public:
     double switchCase(int choice, vector<double> &arr) {
