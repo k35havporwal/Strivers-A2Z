bf972ac5ad3fa1579dcc38dc3e104398282197f1
k35havporwal <145449003+k35havporwal@users.noreply.github.com>
2024-07-25 14:19:39 +0530

    Update 4. For loops.cpp

--- Step-1 : Learn Basics/4. For loops.cpp
+++ Step-1 : Learn Basics/4. For loops.cpp
@@ -1,2 +1,23 @@
-Nth Fibonacci Number 
-// use recursion
+//Nth Fibonacci Number 
+
+#include <iostream>
+using namespace std;
+
+int main (){
+  int fib1=0;
+  int fib2=1;
+  int n,newfib=0;
+  cout << "Enter Value of N : ";
+  cin >> n;
+  if ( n==1 || n==2 ){
+    cout << fib2;
+  }
+  else{
+  for (int i=1 ; i<n ; i++){
+      newfib = fib1+fib2;
+      fib1=fib2;
+      fib2=newfib;
+  }
+  cout << newfib;
+  }
+}
