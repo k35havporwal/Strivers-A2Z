d33e0a4b4622ca4038e23f81c092be393fc844bb
k35havporwal <145449003+k35havporwal@users.noreply.github.com>
2024-07-25 12:18:24 +0530

    Create 5. While loops.cpp

---
+++ Step-1 : Learn Basics/5. While loops.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+int main(){
+  int n;
+  cout << "Enter Value Of N : ";
+  cin >> n;
+  int i=0;
+  while ( i < 10){
+    int num = (n*10)-(n*i);
+    cout << num << endl;
+    i++;
+  }
+}
